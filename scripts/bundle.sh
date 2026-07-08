64e85ff77e78bf726b4f94b4393ce867cff2ee89
Sophia Al-Shwauva <s.alshwauva@gmail.com>
2026-07-08 16:02:35 -0400

    add a real app icon: a pixel-art mug of mead

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -13,6 +13,7 @@ APP="build/mead.app"
 rm -rf "$APP"
 mkdir -p "$APP/Contents/MacOS" "$APP/Contents/Resources"
 cp "$BIN" "$APP/Contents/MacOS/mead"
+cp "Resources/AppIcon.icns" "$APP/Contents/Resources/AppIcon.icns"
 
 cat > "$APP/Contents/Info.plist" <<'PLIST'
 <?xml version="1.0" encoding="UTF-8"?>
@@ -23,6 +24,7 @@ cat > "$APP/Contents/Info.plist" <<'PLIST'
   <key>CFBundleDisplayName</key><string>mead</string>
   <key>CFBundleIdentifier</key><string>com.sophia.mead</string>
   <key>CFBundleExecutable</key><string>mead</string>
+  <key>CFBundleIconFile</key><string>AppIcon</string>
   <key>CFBundlePackageType</key><string>APPL</string>
   <key>CFBundleVersion</key><string>0.0.0</string>
   <key>CFBundleShortVersionString</key><string>0.0</string>
